867b0e1f876796c82d3ccfc2c34885596a901b0a
Emiliano2123 <emilianodc@ciencias.unam.mx>
2024-03-18 19:32:31 -0600

    Los filosofos comen

--- Practica03/src/main/java/unam/ciencias/computoconcurrente/Filosofo.java
+++ Practica03/src/main/java/unam/ciencias/computoconcurrente/Filosofo.java
@@ -1,6 +1,7 @@
 package unam.ciencias.computoconcurrente;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Cada filósofo se ejecuta en un hilo.
@@ -19,18 +20,82 @@ public class Filosofo implements Runnable {
     }
 
     public void run() {
-        //Aqui va tu codigo
+        while(!haComido){
+            //Cada filosofo piensa por un tiempo aleatorio
+            try{
+                pensar();
+            }catch(InterruptedException e){
+                e.printStackTrace(System.out);
+            }
+            //Todos los filosofos menos uno son diestros
+            if(id != 0){
+                //Intenta tomar palillo izquierdo
+                try{
+                    palilloIzquierdo.acquire();
+                }catch(Exception e) {
+                    e.printStackTrace(System.out);
+                }
+                System.out.println("Filosofo  " + (id+1) + " levanta el palillo izquierdo.");
+                //Intenta tomar palillo derecho
+                try{
+                    palilloDerecho.acquire();
+                }catch(Exception e) {
+                    e.printStackTrace(System.out);
+                }
+                System.out.println("Filosofo  " + (id+1) + " levanta el palillo derecho.");
+            }else{
+                //Intenta tomar palillo derecho
+                try{
+                    palilloDerecho.acquire();
+                }catch(Exception e) {
+                    e.printStackTrace(System.out);
+                }
+                System.out.println("Filosofo  " + (id+1) + " levanta el palillo derecho.");
+                //Intenta tomar palillo izquierdo
+                try{
+                    palilloIzquierdo.acquire();
+                }catch(Exception e) {
+                    e.printStackTrace(System.out);
+                }
+                System.out.println("Filosofo  " + (id+1) + " levanta el palillo izquierdo.");
+            }
+            //Intentan comer
+            try{
+                comer();
+            }catch(InterruptedException e){
+                e.printStackTrace(System.out);
+            }
+            //Dejan ambos palillos
+            palilloIzquierdo.release();
+            System.out.println("Filosofo  " + (id+1) + " deja el palillo izquierdo.");
+            palilloDerecho.release();
+            System.out.println("Filosofo  " + (id+1) + " deja el palillo derecho.");
+        }
     }
 
     private void pensar() throws InterruptedException {
-        //Aqui va tu codigo
+        try{
+            int sleepTime = ThreadLocalRandom.current().nextInt(0, 1000);  
+            System.out.println("Filosofo " + (id + 1) + " piensa por " + sleepTime +"ms");
+            Thread.sleep(sleepTime);  
+        }catch (Exception e){  
+            e.printStackTrace(System.out);  
+        }
     }
 
     private void comer() throws InterruptedException {
-        //Aqui va tu codigo
+        try{
+            int sleepTime = ThreadLocalRandom.current().nextInt(0, 1000);  
+            System.out.println("Filosofo " + (id + 1) + " come por " + sleepTime +"ms");
+            Thread.sleep(sleepTime);  
+            haComido = true;
+        }catch (Exception e){  
+            e.printStackTrace(System.out);  
+        }
     }
 
     public boolean haComido() {
         return haComido;
     }
+
 }
